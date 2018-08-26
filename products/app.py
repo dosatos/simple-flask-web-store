75b80bf42481cbf89049f4f86e5d3e26771ded3b
yb2n17@soton.ac.uk <yb2n17@soton.ac.uk>
2018-08-26 17:46:32 +0100

    config object added to the app

--- products/app.py
+++ products/app.py
@@ -1,9 +1,9 @@
-from flask import Blueprint
-
-
 # ============== #
 #  Products App  #
 # ============== #
+
+from flask import Blueprint
+
 
 products_app = Blueprint("products_app", __name__)
 
--- web/app.py
+++ web/app.py
@@ -1,13 +1,16 @@
-from flask import Flask, redirect, url_for
-from products.app import products_app
-
 # ============== #
 #    Main App    #
 # ============== #
 
+from flask import Flask, redirect, url_for
+
+from web import config
+from products.app import products_app
+
+
 app = Flask(__name__)
 app.register_blueprint(products_app, url_prefix="/products")
-
+app.config.from_object(config.DevelopmentConfig)
 
 @app.route("/")
 def index():
