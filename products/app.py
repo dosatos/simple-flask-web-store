54bad2561d41896527479fb5ad29e3df1958db09
yb2n17@soton.ac.uk <yb2n17@soton.ac.uk>
2018-08-27 00:24:52 +0100

    single product page view refactored: name changed to a more explicit, connected to db

--- products/app.py
+++ products/app.py
@@ -5,6 +5,7 @@
 from flask import Blueprint, render_template
 
 from web.database import init_db
+from web.database import db_session
 from products.models import Product
 
 
@@ -23,17 +24,17 @@ def show_products():
 
 
 @products_app.route("/<int:product_id>", methods=["GET", "POST"])
-def show_product(product_id):
+def show_single_product(product_id):
+    product = db_session.query(Product).get(product_id)
     tempalte_path = "products/product.html"
     context = {
-        "product_id": product_id,
+        "product": product,
     }
     return render_template(tempalte_path, **context)
 
 
 # @products_app.route("/add_products", methods=["GET"])
 # def add_products():
-#     from web.database import db_session
 #     db_session.add(Product('Computer', 15, 'Mac book pro - a product of Apple. Nice looking and easy to use'))
 #     db_session.add(Product('Smartphone', 10, 'Android OnePlus5 - quite interesting purchase in this month'))
 #     db_session.add(Product('Watches', 5, 'Golden watches with GPS tracker and calories counter'))
