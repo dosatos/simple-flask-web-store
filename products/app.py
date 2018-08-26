fbd0bc498ed8eb3bdc0857a18622a1241d1641bd
yb2n17@soton.ac.uk <yb2n17@soton.ac.uk>
2018-08-26 19:12:56 +0100

    href from list of products to one product page added

--- products/app.py
+++ products/app.py
@@ -12,15 +12,6 @@ products_app = Blueprint("products_app", __name__, template_folder="templates/")
 init_db()
 
 
-@products_app.route("/add_products", methods=["GET"])
-def add_products():
-    db_session.add(Product('Computer', 15, 'Mac book pro - a product of Apple. Nice looking and easy to use'))
-    db_session.add(Product('Smartphone', 10, 'Android OnePlus5 - quite interesting purchase in this month'))
-    db_session.add(Product('Watches', 5, 'Golden watches with GPS tracker and calories counter'))
-    db_session.commit()
-    return "Items added"
-
-
 @products_app.route("/", methods=["GET"])
 def show_products():
     products = Product.query.all()
@@ -38,3 +29,12 @@ def show_product(product_id):
         "product_id": product_id,
     }
     return render_template(tempalte_path, **context)
+
+
+# @products_app.route("/add_products", methods=["GET"])
+# def add_products():
+#     db_session.add(Product('Computer', 15, 'Mac book pro - a product of Apple. Nice looking and easy to use'))
+#     db_session.add(Product('Smartphone', 10, 'Android OnePlus5 - quite interesting purchase in this month'))
+#     db_session.add(Product('Watches', 5, 'Golden watches with GPS tracker and calories counter'))
+#     db_session.commit()
+#     return "Items added"
